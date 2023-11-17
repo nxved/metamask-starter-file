6bd7cd8fd4541b1ad36a483f8267c9e31bd76e57
unknown <benaved@gmail.com>
2023-11-17 12:38:07 +0530

    intial

---
+++ popup.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function () {});
+
+let providerURL = "";
+let provider;
+let privateKey;
+let address;
+
+function handler() {}
+
+function checkBlance() {}
+
+function getOpenNetwork() {}
+
+function getSelectedNetwork() {}
+
+function setNetwork() {}
+
+function loginUser() {}
+
+function createUser() {}
+
+function openCreate() {}
+
+function signUp() {}
+
+function login() {}
+
+function logout() {}
+
+function openTransfer() {}
+
+function goBack() {}
+
+function openImport() {}
+
+function importGoBack() {}
+
+function openActiviy() {}
+
+function openAssets() {}
+
+function goHomePage() {}
+
+function openImportModel() {}
+
+function closeImportModel() {}
+
+function addToken() {}
+
+function addAccount() {}
+
+function myFunction() {}
+
+function copyAddress() {}
+
+function changeAccount() {}
